7350ad085ed34c98673701477b66c45db8720de0
Marcos Vinicius Lima <marcoslimaraimundo@gmail.com>
2019-11-17 14:14:53 -0300

    Create find_fatorial_of_a_number_using_while_loop.java

---
+++ find_fatorial_of_a_number_using_while_loop.java
@@ -0,0 +1,11 @@
+public class Factorial {
+    public static void main(String[] args) {
+        int num = 5, i = 1;
+        long factorial = 1;
+        while (i <= num) {
+            factorial *= i;
+            i++;
+        }
+        System.out.printf("Factorial of %d = %d", num, factorial);
+    }
+}
