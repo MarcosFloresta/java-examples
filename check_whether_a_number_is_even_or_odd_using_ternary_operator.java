42c84b8864cdde1d9d40af28840418629ff4ff47
Marcos Vinicius Lima <marcoslimaraimundo@gmail.com>
2019-11-17 14:15:53 -0300

    Create check_whether_a_number_is_even_or_odd_using_ternary_operator.java

---
+++ check_whether_a_number_is_even_or_odd_using_ternary_operator.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = reader.nextInt();
+        String evenOdd = (num % 2 == 0) ? "even" : "odd";
+        System.out.println(num + " is " + evenOdd);
+    }
+}
