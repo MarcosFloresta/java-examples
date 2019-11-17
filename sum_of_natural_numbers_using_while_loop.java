21694b2f123d4227e7f02399df9cdce872703bec
Marcos Vinicius Lima <marcoslimaraimundo@gmail.com>
2019-11-17 14:12:32 -0300

    Create sum_of_natural_numbers_using_while_loop.java

---
+++ sum_of_natural_numbers_using_while_loop.java
@@ -0,0 +1,10 @@
+public class SumNatural {
+    public static void main(String[] args) {
+        int num = 50, i = 1, sum = 0;
+        while (i <= num) {
+            sum += i;
+            i++;
+        }
+        System.out.println("Sum = " + sum);
+    }
+}
