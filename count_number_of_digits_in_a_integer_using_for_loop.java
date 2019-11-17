53de98cc7df23db459908926c1a81c6c64300657
Marcos Vinicius Lima <marcoslimaraimundo@gmail.com>
2019-11-17 14:15:30 -0300

    Create count_number_of_digits_in_a_integer_using_for_loop.java

---
+++ count_number_of_digits_in_a_integer_using_for_loop.java
@@ -0,0 +1,8 @@
+public class NumberDigits {
+    public static void main(String[] args) {
+        int count = 0, num = 123456;
+        for (; num != 0; num /= 10, ++count) {
+        }
+        System.out.println("Number of digits: " + count);
+    }
+}
