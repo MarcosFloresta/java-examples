4efb9f027722af19b5b3ccb1509337d6df9c7053
Marcos Vinicius Lima <marcoslimaraimundo@gmail.com>
2019-11-18 19:31:11 -0300

    Create convert_array_to_set.java

---
+++ convert_array_to_set.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class ArraySet {
+    public static void main(String[] args) {
+        String[] array = { "a", "b", "c" };
+        Set<String> set = new HashSet<>(Arrays.asList(array));
+        System.out.println("Set: " + set);
+    }
+}
