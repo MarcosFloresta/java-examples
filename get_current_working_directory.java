e250b2afdc708553973a54465836deec00c20eed
Marcos Vinicius Lima <marcoslimaraimundo@gmail.com>
2019-11-18 19:30:17 -0300

    Create get_current_working_directory.java

---
+++ get_current_working_directory.java
@@ -0,0 +1,7 @@
+public class CurrDirectory {
+    public static void main(String[] args) {
+        String path = System.getProperty("user.dir");
+
+        System.out.println("Working Directory = " + path);
+    }
+}
