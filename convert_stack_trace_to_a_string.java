7a322d98e6e086ca34a7982ced0b2feb2e0f5d8c
Marcos Vinicius Lima <marcoslimaraimundo@gmail.com>
2019-11-18 19:30:36 -0300

    Create convert_stack_trace_to_a_string.java

---
+++ convert_stack_trace_to_a_string.java
@@ -0,0 +1,15 @@
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+public class PrintStackTrace {
+    public static void main(String[] args) {
+        try {
+            int division = 0 / 0;
+        } catch (ArithmeticException e) {
+            StringWriter sw = new StringWriter();
+            e.printStackTrace(new PrintWriter(sw));
+            String exceptionAsString = sw.toString();
+            System.out.println(exceptionAsString);
+        }
+    }
+}
