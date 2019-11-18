8b2d86d2808c5bb5e45e0459371a481665655189
Marcos Vinicius Lima <marcoslimaraimundo@gmail.com>
2019-11-18 19:30:58 -0300

    Create convert_char_array_to_string.java

---
+++ convert_char_array_to_string.java
@@ -0,0 +1,9 @@
+public class CharString {
+    public static void main(String[] args) {
+        char[] ch = { 'a', 'e', 'i', 'o', 'u' };
+        String st = String.valueOf(ch);
+        String st2 = new String(ch);
+        System.out.println(st);
+        System.out.println(st2);
+    }
+}
