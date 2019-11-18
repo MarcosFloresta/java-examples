eb2f532552a6452bd08a4bc52059123dfd8f5b90
Marcos Vinicius Lima <marcoslimaraimundo@gmail.com>
2019-11-18 19:31:21 -0300

    Create compare_two_strings_objects_using_==(doesn't_work).java

---
+++ compare_two_strings_objects_using_==(doesn't_work).java
@@ -0,0 +1,10 @@
+public class CompareStrings {
+    public static void main(String[] args) {
+        String style = new String("Bold");
+        String style2 = new String("Bold");
+        if (style == style2)
+            System.out.println("Equal");
+        else
+            System.out.println("Not Equal");
+    }
+}
