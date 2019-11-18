10619cc57a00fcf2c14162f0315564f8a9c2199f
Marcos Vinicius Lima <marcoslimaraimundo@gmail.com>
2019-11-18 19:31:13 -0300

    Create convert_array_to_list.java

---
+++ convert_array_to_list.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayToList {
+    public static void main(String[] args) {
+        String[] array = { "a", "b" };
+        List<String> list = Arrays.asList(array);
+        System.out.println(list);
+    }
+}
