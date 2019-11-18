41d85fa793674581e2649f36dd8cf2ba42ad9ea6
Marcos Vinicius Lima <marcoslimaraimundo@gmail.com>
2019-11-18 19:29:56 -0300

    Create print_an_array_using_standard_library_arrays.java

---
+++ print_an_array_using_standard_library_arrays.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class Array {
+    public static void main(String[] args) {
+        int[] array = { 1, 2, 3, 4, 5 };
+        System.out.println(Arrays.toString(array));
+    }
+}
